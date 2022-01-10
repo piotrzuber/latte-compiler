856a07624b93b0b0cfab4705c566f5bce7cd7448
Piotr Żuber <piotr.a.zuber@gmail.com>
2022-01-10 16:42:05 +0100

    ??

---
+++ lib/runtime.s
@@ -0,0 +1,289 @@
+	.file	"runtime.c"
+	.text
+	.globl	concat
+	.type	concat, @function
+concat:
+.LFB0:
+	.cfi_startproc
+	endbr32
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	pushl	%ebx
+	subl	$20, %esp
+	.cfi_offset 3, -12
+	call	__x86.get_pc_thunk.bx
+	addl	$_GLOBAL_OFFSET_TABLE_, %ebx
+	subl	$12, %esp
+	pushl	8(%ebp)
+	call	strlen@PLT
+	addl	$16, %esp
+	movl	%eax, -12(%ebp)
+	subl	$12, %esp
+	pushl	12(%ebp)
+	call	strlen@PLT
+	addl	$16, %esp
+	movl	%eax, -16(%ebp)
+	movl	-12(%ebp), %edx
+	movl	-16(%ebp), %eax
+	addl	%edx, %eax
+	addl	$1, %eax
+	subl	$12, %esp
+	pushl	%eax
+	call	malloc@PLT
+	addl	$16, %esp
+	movl	%eax, -20(%ebp)
+	subl	$4, %esp
+	pushl	-12(%ebp)
+	pushl	8(%ebp)
+	pushl	-20(%ebp)
+	call	memcpy@PLT
+	addl	$16, %esp
+	movl	-20(%ebp), %edx
+	movl	-12(%ebp), %eax
+	addl	%edx, %eax
+	subl	$4, %esp
+	pushl	-16(%ebp)
+	pushl	12(%ebp)
+	pushl	%eax
+	call	memcpy@PLT
+	addl	$16, %esp
+	movl	-12(%ebp), %edx
+	movl	-16(%ebp), %eax
+	addl	%eax, %edx
+	movl	-20(%ebp), %eax
+	addl	%edx, %eax
+	movb	$0, (%eax)
+	movl	-20(%ebp), %eax
+	movl	-4(%ebp), %ebx
+	leave
+	.cfi_restore 5
+	.cfi_restore 3
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	concat, .-concat
+	.section	.rodata
+.LC0:
+	.string	"runtime error"
+	.text
+	.globl	error
+	.type	error, @function
+error:
+.LFB1:
+	.cfi_startproc
+	endbr32
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	pushl	%ebx
+	subl	$4, %esp
+	.cfi_offset 3, -12
+	call	__x86.get_pc_thunk.bx
+	addl	$_GLOBAL_OFFSET_TABLE_, %ebx
+	subl	$12, %esp
+	leal	.LC0@GOTOFF(%ebx), %eax
+	pushl	%eax
+	call	puts@PLT
+	addl	$16, %esp
+	subl	$12, %esp
+	pushl	$1
+	call	exit@PLT
+	.cfi_endproc
+.LFE1:
+	.size	error, .-error
+	.section	.rodata
+.LC1:
+	.string	"%d\n"
+	.text
+	.globl	printInt
+	.type	printInt, @function
+printInt:
+.LFB2:
+	.cfi_startproc
+	endbr32
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	pushl	%ebx
+	subl	$4, %esp
+	.cfi_offset 3, -12
+	call	__x86.get_pc_thunk.ax
+	addl	$_GLOBAL_OFFSET_TABLE_, %eax
+	subl	$8, %esp
+	pushl	8(%ebp)
+	leal	.LC1@GOTOFF(%eax), %edx
+	pushl	%edx
+	movl	%eax, %ebx
+	call	printf@PLT
+	addl	$16, %esp
+	nop
+	movl	-4(%ebp), %ebx
+	leave
+	.cfi_restore 5
+	.cfi_restore 3
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	printInt, .-printInt
+	.globl	printString
+	.type	printString, @function
+printString:
+.LFB3:
+	.cfi_startproc
+	endbr32
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	pushl	%ebx
+	subl	$4, %esp
+	.cfi_offset 3, -12
+	call	__x86.get_pc_thunk.ax
+	addl	$_GLOBAL_OFFSET_TABLE_, %eax
+	subl	$12, %esp
+	pushl	8(%ebp)
+	movl	%eax, %ebx
+	call	puts@PLT
+	addl	$16, %esp
+	nop
+	movl	-4(%ebp), %ebx
+	leave
+	.cfi_restore 5
+	.cfi_restore 3
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE3:
+	.size	printString, .-printString
+	.globl	readInt
+	.type	readInt, @function
+readInt:
+.LFB4:
+	.cfi_startproc
+	endbr32
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	pushl	%ebx
+	subl	$20, %esp
+	.cfi_offset 3, -12
+	call	__x86.get_pc_thunk.ax
+	addl	$_GLOBAL_OFFSET_TABLE_, %eax
+	subl	$8, %esp
+	leal	-12(%ebp), %edx
+	pushl	%edx
+	leal	.LC1@GOTOFF(%eax), %edx
+	pushl	%edx
+	movl	%eax, %ebx
+	call	__isoc99_scanf@PLT
+	addl	$16, %esp
+	movl	-12(%ebp), %eax
+	movl	-4(%ebp), %ebx
+	leave
+	.cfi_restore 5
+	.cfi_restore 3
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE4:
+	.size	readInt, .-readInt
+	.globl	readString
+	.type	readString, @function
+readString:
+.LFB5:
+	.cfi_startproc
+	endbr32
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	pushl	%ebx
+	subl	$20, %esp
+	.cfi_offset 3, -12
+	call	__x86.get_pc_thunk.bx
+	addl	$_GLOBAL_OFFSET_TABLE_, %ebx
+	movl	$0, -12(%ebp)
+	movl	stdin@GOT(%ebx), %eax
+	movl	(%eax), %eax
+	subl	$4, %esp
+	pushl	%eax
+	leal	-16(%ebp), %eax
+	pushl	%eax
+	leal	-12(%ebp), %eax
+	pushl	%eax
+	call	getline@PLT
+	addl	$16, %esp
+	movl	-12(%ebp), %eax
+	subl	$12, %esp
+	pushl	%eax
+	call	strlen@PLT
+	addl	$16, %esp
+	movl	%eax, -16(%ebp)
+	movl	-12(%ebp), %eax
+	movl	-16(%ebp), %edx
+	subl	$1, %edx
+	addl	%edx, %eax
+	movb	$0, (%eax)
+	movl	-12(%ebp), %eax
+	movl	-4(%ebp), %ebx
+	leave
+	.cfi_restore 5
+	.cfi_restore 3
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE5:
+	.size	readString, .-readString
+	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
+	.globl	__x86.get_pc_thunk.ax
+	.hidden	__x86.get_pc_thunk.ax
+	.type	__x86.get_pc_thunk.ax, @function
+__x86.get_pc_thunk.ax:
+.LFB6:
+	.cfi_startproc
+	movl	(%esp), %eax
+	ret
+	.cfi_endproc
+.LFE6:
+	.section	.text.__x86.get_pc_thunk.bx,"axG",@progbits,__x86.get_pc_thunk.bx,comdat
+	.globl	__x86.get_pc_thunk.bx
+	.hidden	__x86.get_pc_thunk.bx
+	.type	__x86.get_pc_thunk.bx, @function
+__x86.get_pc_thunk.bx:
+.LFB7:
+	.cfi_startproc
+	movl	(%esp), %ebx
+	ret
+	.cfi_endproc
+.LFE7:
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 4
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 4
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 4
+4:
